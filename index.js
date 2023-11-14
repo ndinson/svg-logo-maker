89dea64d53bcde294796e28e8fc0ee5738faac7a
Nainoa Dinson <nd808@msn.com>
2023-11-14 06:53:12 -1000

    installed inquirer package

--- index.js
+++ index.js
@@ -6,3 +6,22 @@
 // TODO: Create a prompt for the shape color (using keyword or hexadecimal number).
 // TODO: Add function that creates an SVG file named `logo.svg`, after all questions have been answered.
 // TODO: Create output text "Generated logo.svg" in the command line.
+
+// Package used for collectiing input from the user.
+const inquirer = require('inquirer');
+
+// Prompt of questions that collect input from user, the criteria of logo being created.
+inquirer.prompt ([
+  {
+    type: 'input',
+    message: 'Enter up to 3 characters that will represent your logo.',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'Select a shape for your logo.',
+    name: 'shape',
+    choices: ['Square', 'Circle', 'Triangle']
+  }
+
+])
