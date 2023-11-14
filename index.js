8bdfc095d2af15536d2e31816da2e82df29e1402
Nainoa Dinson <nd808@msn.com>
2023-11-14 06:20:10 -1000

    added TODO list based off of acceptance criteria for this homework assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+// TODO: Create application which generates a 300x200 pixel image in browser based off of the criteria entered.
+// TODO: Create a basic instructional prompt for using app.
+// TODO: Create a prompt to enter up to three characters.
+// TODO: Create a prompt for the text color (using keyword or hexadecimal number).
+// TODO: Create a prompt for the shape.
+// TODO: Create a prompt for the shape color (using keyword or hexadecimal number).
+// TODO: Add function that creates an SVG file named `logo.svg`, after all questions have been answered.
+// TODO: Create output text "Generated logo.svg" in the command line.
